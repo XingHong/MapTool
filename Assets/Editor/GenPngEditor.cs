ecc11f33b3c60cdb0c5f5131bda7141655cf5e47
XingHong <475773414@qq.com>
2023-04-21 11:11:16 +0800

    调整生成palette流程

--- Assets/Editor/GenPngEditor.cs
+++ Assets/Editor/GenPngEditor.cs
@@ -38,20 +38,31 @@ public class GenPngEditor
     [MenuItem("MapTool/Test")]
     static void ImportExample()
     {
-        var owner = GridPaintPaletteWindow.instances[0];
-        owner.Focus();
-
         GameObject go = GridPaletteUtility.CreateNewPalette(PalettesDir, "test", GridLayout.CellLayout.Isometric, GridPalette.CellSizing.Manual, new Vector3(1, 0.5f)
             , GridLayout.CellSwizzle.XYZ, TransparencySortMode.Default, new Vector3(0, 0, 1f));
+        var owner = GridPaintPaletteWindow.instances.Count > 0 ? GridPaintPaletteWindow.instances[0] : null;
+        if (owner != null)
+            owner.Focus();
         if (go != null)
         {
             Tilemap tm = go.GetComponentInChildren<Tilemap>();
             tm.tileAnchor = new Vector3(1, 1, 0);
             var tile = (TileBase)AssetDatabase.LoadAssetAtPath("Assets/Tiles/base00.asset", typeof(TileBase));
             tm.SetTile(Vector3Int.zero, tile);
-            owner.palette = go;
-            owner.Repaint();
-            owner.SavePalette();
+            if (owner != null)
+            {
+                owner.palette = go;
+                owner.Repaint();
+                owner.SavePalette();
+            }
+            else
+            {
+                string path = AssetDatabase.GetAssetPath(go);
+                var instance = GameObject.Instantiate<GameObject>(go);
+                PrefabUtility.SaveAsPrefabAssetAndConnect(instance, path, InteractionMode.AutomatedAction);
+                GameObject.DestroyImmediate(instance);               
+                AssetDatabase.Refresh();
+            }
         }
     }
 
